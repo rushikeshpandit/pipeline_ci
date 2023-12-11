eff771fc0f33c88faa108ee253f667820f94d299
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-12-11 19:38:21 +0530

    - Added "Dialyxir" for Mix tasks to simplify use of Dialyzer

--- mix.exs
+++ mix.exs
@@ -61,7 +61,8 @@ defmodule PipelineCi.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
-      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
     ]
   end
 
