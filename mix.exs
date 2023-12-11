1ab2d0e95aca7de3602088fde5941c4cbc3b42d9
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-12-11 19:34:47 +0530

    - Added "SoBelow" for Security-focused static analysis for the Phoenix Framework

--- mix.exs
+++ mix.exs
@@ -60,7 +60,8 @@ defmodule PipelineCi.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.18", only: :test}
+      {:excoveralls, "~> 0.18", only: :test},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
     ]
   end
 
