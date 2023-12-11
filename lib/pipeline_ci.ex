45d613edc4e93abfc00d699243739c28f5789658
Rushikesh Pandit <13253098+rushikeshpandit@users.noreply.github.com>
2023-12-11 20:26:23 +0530

    - Sample commit (#1)
    * - Sample commit
    
    * - Renamed "workflow" folder to "workflows" inside .github directory

--- lib/pipeline_ci.ex
+++ lib/pipeline_ci.ex
@@ -2,7 +2,7 @@ defmodule PipelineCi do
   @moduledoc """
   PipelineCi keeps the contexts that define your domain
   and business logic.
-
+  Sample text
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
